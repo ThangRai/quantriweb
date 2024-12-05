f5eb90976d1f7c50636512b3c289173fb43d548e
ThangRai <132726379+ThangRai@users.noreply.github.com>
2024-12-05 14:16:24 +0700

    Update add_sanpham.php

--- quantriweb/add_sanpham.php
+++ quantriweb/add_sanpham.php
@@ -767,7 +767,6 @@ $conn->close();
 
         /* Các input trường */
         input[type="text"], input[type="number"], input[type="file"], button {
-            width: 100%;
             padding: 10px;
             margin: 10px 0;
             border-radius: 5px;
