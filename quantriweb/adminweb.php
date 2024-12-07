69dac3ae49b71af6e69b8b41de04113cdaaf2d7d
Thắng Rai <badaotulong123@gmail.com>
2024-12-07 11:21:21 +0700

    Update

--- quantriweb/adminweb.php
+++ quantriweb/adminweb.php
@@ -30,6 +30,7 @@ $role = $_SESSION['role'];  // Nếu bạn cần lấy thông tin vai trò ngư�
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
+    <link rel="icon" href="uploads/favicon.png" type="image/png">
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
